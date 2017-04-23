8c538cc4ece10466b8692e480eb46ffe5371aa01
Alex Howington <alex.howington@gmail.com>
2017-04-22 18:15:10 -0600

    Style donut chart with reactstrap

--- client/app/bundles/HelloWorld/components/D3DonutChart.jsx
+++ client/app/bundles/HelloWorld/components/D3DonutChart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Motion, spring} from 'react-motion';
 import d3 from "d3";
+import { Container, Row, Col, Card, Button, CardTitle, CardText } from 'reactstrap';
 
 export default class D3DonutChart extends React.Component {
   constructor(props, _railsContext) {
@@ -50,11 +51,11 @@ export default class D3DonutChart extends React.Component {
   };
 
   render() {
-    var width = 640,
+    var width = 300,
         height = 300,
         radius = Math.min(width, height) / 2;
     
-    var color = d3.scale.category20();
+    var color = d3.scale.category10();
     
     var pie = d3.layout.pie()
         .value(d => d)
@@ -67,29 +68,49 @@ export default class D3DonutChart extends React.Component {
     var displayedData = pie(this.state.data);
         
     return <div>
-        <svg width={width} height={height} onClick={this.randomData}>
-          <g transform={"translate(" + width / 2 + "," + height / 2 + ")"}>
-            {displayedData.map((slice, i) =>
-              <Motion
-                key={i}
-                defaultStyle={{
-                  startAngle: slice.startAngle,
-                  endAngle: slice.endAngle,
-                  padAngle: slice.padAngle,
-                }}
-                style={{
-                  startAngle: spring(slice.startAngle),
-                  endAngle: spring(slice.endAngle),
-                  padAngle: spring(slice.padAngle)
-                }}>{value => <path
-                  fill={color(i)}
-                  d={arc(value)} />
-                }</Motion>
-            )}
-          </g>
-        </svg>
-        <p>X: {this.state.data[0]}</p>
-        <p>O: {this.state.data[1]}</p>
+        <Container>
+          <Row>
+            <Col md={{ size: 6, offset: 3 }}>
+              <Card block>
+                <CardTitle className="text-center">Wins</CardTitle>
+                <CardText className="text-center">
+                  <svg width={width} height={height} onClick={this.randomData}>
+                    <g transform={"translate(" + width / 2 + "," + height / 2 + ")"}>
+                      {displayedData.map((slice, i) =>
+                        <Motion
+                          key={i}
+                          defaultStyle={{
+                            startAngle: slice.startAngle,
+                            endAngle: slice.endAngle,
+                            padAngle: slice.padAngle,
+                          }}
+                          style={{
+                            startAngle: spring(slice.startAngle),
+                            endAngle: spring(slice.endAngle),
+                            padAngle: spring(slice.padAngle)
+                          }}>{value => <path
+                            fill={color(i)}
+                            d={arc(value)} />
+                          }</Motion>
+                      )}
+                    </g>
+                  </svg>
+                </CardText>
+                <Row>
+                  <Col sm="6" className="text-right">
+                    <h3 className="x-label">X:</h3>
+                    <h3 className="o-label">O:</h3>
+                  </Col>
+                  <Col sm="6">
+                    <h3>{this.state.data[0]}</h3>
+                    <h3>{this.state.data[1]}</h3>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+        
       </div>;
   }
 }

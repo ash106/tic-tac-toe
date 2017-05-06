8a4c142f75bd80a79caa7708021248fe48b3251d
Alex Howington <alex.howington@gmail.com>
2017-05-06 03:55:31 -0600

    Remove unnecessary React on Rails registration code

--- client/app/bundles/Stats/startup/registration.jsx
+++ client/app/bundles/Stats/startup/registration.jsx
@@ -1,13 +1,7 @@
 import ReactOnRails from 'react-on-rails';
 
 import StatsContainer from '../containers/StatsContainer';
-import Stats from '../components/Stats';
-import D3DonutChart from '../components/D3DonutChart';
-import ChartLabel from '../components/ChartLabel';
 
 ReactOnRails.register({
-  StatsContainer,
-  Stats,
-  D3DonutChart,
-  ChartLabel
+  StatsContainer
 });
--- client/app/bundles/TicTacToe/startup/registration.jsx
+++ client/app/bundles/TicTacToe/startup/registration.jsx
@@ -2,19 +2,9 @@ import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../components/HelloWorld';
 import GameContainer from '../containers/GameContainer';
-import Game from '../components/Game';
-import Board from '../components/Board';
-import Square from '../components/Square';
-import StatusLabel from '../components/StatusLabel';
-import RestartButton from '../components/RestartButton';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorld,
-  GameContainer,
-  Game,
-  Board,
-  Square,
-  StatusLabel,
-  RestartButton
+  GameContainer
 });

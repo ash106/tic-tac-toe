fece0829f9f13e0d744315cb0a6528eb44205d9b
Alex Howington <alex.howington@gmail.com>
2017-05-06 18:23:57 -0600

    Highlight the next player or winner in the StatusLabel

--- client/app/bundles/TicTacToe/components/StatusLabel.jsx
+++ client/app/bundles/TicTacToe/components/StatusLabel.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 export default class StatusLabel extends React.Component {
-  render() {    
+  render() {
+    var status = this.props.status.split('');
+    var spanText = status.pop();
+    var highlightClass = spanText == "X" ? "x-span" : "o-span";
+    if (status.length < 4) {
+      highlightClass = "";
+    }
+    status = status.join('');
     return (
-      <h1 className="status text-center">{this.props.status}</h1>
+      <h1 className="status text-center">{status}<span className={highlightClass}>{spanText}</span></h1>
     );
   }
 }
